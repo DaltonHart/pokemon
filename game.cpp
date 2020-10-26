969ed1e0c3b23ae5b47f33c0d5ff96fb4a6e5a38
Dalton <daltonvaren@gmail.com>
2020-10-26 15:43:28 -0700

    adds display moves

--- game.cpp
+++ game.cpp
@@ -15,14 +15,25 @@ class Pokemon
 public:
   std::string name;
   int level;
-
   Move moves[4];
+
+  void displayMoves()
+  {
+    size_t i = 0;
+    for (i = 0; i < 4; i++)
+    {
+      std::cout << "======\n";
+
+      std::cout << moves[i].title << "\n";
+    }
+  };
 };
 
 int main()
 {
-  Pokemon charmander = {"Charmander", 1, {{"scratch", 10, 10}}};
+  Pokemon charmander = {"Charmander", 1, {{"scratch", 10, 10}, {"tail wag", 15, 15}}};
 
   std::cout << charmander.name << "\n";
+  charmander.displayMoves();
   return 0;
 }
