b13206a2bb740db651f1716c178392be52c72cc4
Dalton <daltonvaren@gmail.com>
2020-10-26 15:18:06 -0700

    adds moves

--- game.cpp
+++ game.cpp
@@ -1,6 +1,14 @@
 #include <string>
 #include <iostream>
 
+class Move
+{
+public:
+  std::string title;
+  int pp_total;
+  int current_pp;
+};
+
 class Pokemon
 {
 
@@ -8,11 +16,12 @@ public:
   std::string name;
   int level;
 
+  Move moves[4];
 };
 
 int main()
 {
-  Pokemon charmander = {"Charmander", 1};
+  Pokemon charmander = {"Charmander", 1, {{"scratch", 10, 10}}};
 
   std::cout << charmander.name << "\n";
   return 0;
